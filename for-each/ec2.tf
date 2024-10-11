952b81cc2339caa45f2042fd1695c47fba892d15
rishikesh <malleswaraws@gmail.com>
2024-10-11 14:41:08 +0530

    functions

---
+++ for-each/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "Expense" {
+    for_each = var.instance_names # each.key and each.value
+    ami = data.aws_ami.ami_info.id
+    vpc_security_group_ids = ["sg-0053ce598ea3ca93b"]
+    instance_type = each.value
+    tags = merge(
+        var.common_tags,
+        {
+            Name = each.key
+            Module = each.key
+            
+        }
+    )
+}
---
+++ for-each/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_names" {
+    type    = map
+    default = {
+        db = "t3.small"
+        backend = "t3.micro"
+        frontend = "t3.micro"
+    }
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        project = "Expense"
+       Terraform = "true"
+    }
+}
